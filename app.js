1780cf9f369a54a2b8f57d1df1fab60adcb1deda
Mirzafar <mirzafar546@gmail.mail>
2023-02-02 19:19:41 +0500

    app

---
+++ app.js
@@ -0,0 +1,241 @@
+// // date
+// let date = new Date(
+//     prompt("Tug'ilgan kuningizni kiriting (Format: MM-dd-yyyy): ")
+//     );
+
+//     let now = Date.now();
+//     let diff = now - date;
+
+//     let year = Math.floor(diff / 1000 / 60 / 60 / 24 / 365.25);
+//     let day = Math.floor(diff / 1000 / 60 / 60 / 24 % 365.5 );
+//     let hour = Math.floor(diff / 1000 / 60 / 60 % 24 );
+
+// alert(`Siz ${year} yil, ${day} kun, ${hour} sog'ot yashabsiz`);
+
+// switch (num) {
+//   case 1:
+//     alert("Dushanba");
+//     break;
+//   case 2:
+//     alert("Seshanba");
+//     break;
+//   case 3:
+//     alert("Chorshanba");
+//     break;
+//   case 4:
+//     alert("Payshanba");
+//     break;
+//   case 5:
+//     alert("Juma");
+//     break;
+//   case 6:
+//     alert("Shanba");
+//     break;
+//   case 7:
+//     alert("Yaksanba");
+//     break;
+
+//   default:
+//     alert("bunday hafta yo'q")
+//     break;
+
+// }
+
+// switch (num) {
+//   case 1:
+//     alert("-Yomon");
+//     break;
+//   case 2:
+//     alert("-Qoniqarsiz");
+//     break;
+//   case 3:
+//     alert("-Qoniqarli");
+//     break;
+//     case 4:
+//       alert("-Yaxshi");
+//       break;
+//       case 5:
+//         alert("-Alo");
+//         break;
+
+//         default:
+//           alert("bunday baho yo'q")
+//           break;
+
+//         }
+
+//       switch (num) {
+// case 1:
+//   alert("Qish");
+//   break;
+// case 2:
+//   alert("Qish");
+//   break;
+// case 3:
+//   alert("Qish");
+//   break;
+//     case 4:
+//       alert("Bahor");
+//       break;
+//     case 5:
+//       alert("Bahor");
+//       break;
+//     case 6:
+//       alert("Bahor");
+//       break;
+//     case 7:
+//       alert("Yoz");
+//       break;
+//     case 8:
+//       alert("Yoz");
+//       break;
+//     case 9:
+//       alert("Yoz");
+//       break;
+//     case 10:
+//       alert("Kuz");
+//       break;
+//     case 11:
+//       alert("Kuz");
+//       break;
+//     case 12:
+//       alert("Kuz");
+//       break;
+
+//       default:
+//         alert("bunday oy yo'q")
+//         break;
+
+//       }
+// let num = +prompt( "oy kiriting" );
+
+//       switch (num) {
+// case 1:
+//   alert("yanvar  31");
+//   break;
+// case 2:
+//   alert("fevral  28");
+//   break;
+// case 3:
+//   alert("mart 30");
+//   break;
+//     case 4:
+//       alert("aprel 31");
+//       break;
+//     case 5:
+//       alert("may 30");
+//       break;
+//     case 6:
+//       alert("iyun 31");
+//       break;
+//     case 7:
+//       alert("iyul 31");
+//       break;
+//     case 8:
+//       alert("avgust 30");
+//       break;
+//     case 9:
+//       alert("sentabr 31");
+//       break;
+//     case 10:
+//       alert("oktabr 31");
+//       break;
+//     case 11:
+//       alert("nayabr 30");
+//       break;
+//     case 12:
+//       alert("dekabr 31");
+//       break;
+
+//       default:
+//         alert("bunday oy yo'q")
+//         break;
+
+//       }
+
+// let miqdor = +prompt("miqdor kiriting")
+// let birlik = prompt("birlikl")
+
+// console.log(miqdor);
+
+// if (birlig === "gram") {
+//   alert(miqdor  / 1000 , "kg")
+// }
+// else if (birlik === "tonna") {
+//   alert(miqdor * 1000 , "kg")
+// }
+
+// let bir = +prompt("yozing (format 1 ... 5 ) ")
+
+// if ( bir === 1) {
+//     alert("Kilogram")
+// }
+
+// else if ( bir === 2  ) {
+//     alert("Milligram")
+// }
+// else if ( bir === 3  ) {
+//     alert("gramm")
+// }
+// else if ( bir === 4  ) {
+//     alert("tonna")
+// }
+
+// else if ( bir === 5  ) {
+//     alert("Sentner")
+// }
+
+// let d = prompt(" d  = kun")
+// let m = prompt(" d  = Oy")
+
+// if ( d === d  ) {
+//     alert(`${d + 1} kengi yil sanasi ${m + m} oy    `);
+// }
+
+// const d = +prompt("");
+
+// if (
+//   d === 1 ||
+//   d === 2 ||
+//   d === 3 ||
+//   d === 4 ||
+//   d === 5 ||
+//   d === 6 ||
+//   d === 7 ||
+//   d === 8 ||
+//   d === 9 ||
+//   d === 10 ||
+//   d === 11 ||
+//   d === 12 ||
+//   d === 13 ||
+//   d === 14 ||
+//   d === 15 ||
+//   d === 16 ||
+//   d === 17 ||
+//   d === 18 ||
+//   d === 19 ||
+//   d === 20 ||
+//   d === 21 ||
+//   d === 22 ||
+//   d === 23 ||
+//   d === 24 ||
+//   d === 25 ||
+//   d === 26 ||
+//   d === 27 ||
+//   d === 28 ||
+//   d === 29 ||
+//   d === 30 ||
+//   d === 31 ||
+//   d === 32 ||
+//   d === 33 ||
+//   d === 34 ||
+//   d === 35 ||
+//   d === 36 ||
+//   d === 37 ||
+//   d === 38 ||
+//   d === 39 ||
+//   d === 40
+// ) {
+//   alert(`${d} ta masala`);
+// }
+
